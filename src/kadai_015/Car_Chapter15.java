8be9dcaaf6f229abf499db185b763516a9915e9c
onaka273 <onaka273@gmail.com>
2025-07-31 07:44:17 +0900

    kadai15完成

--- src/kadai_015/Car_Chapter15.java
+++ src/kadai_015/Car_Chapter15.java
@@ -1,5 +1,25 @@
 package kadai_015;
 
 public class Car_Chapter15 {
+	private int gear  = 1 ;
+	private int speed = 10;
 
+	
+	public void changeGear(int afterGear) {
+		
+		if (afterGear >= 1  &&  afterGear <= 5 ) {
+			gear = afterGear ;
+			speed = afterGear * 10 ;
+		}
+		else {
+			gear = afterGear ;
+			speed = 10 ;
+		}
+	}
+	
+	public void run() {
+		
+		 System.out.println( "現在のギアは" + gear + "、" );
+		 System.out.println( "速度は時速" + speed + "kmです" );
+	}
 }
