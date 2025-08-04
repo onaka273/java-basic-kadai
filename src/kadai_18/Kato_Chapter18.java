21c41cc033309116c5001f7678c657e802383c8a
onaka273 <onaka273@gmail.com>
2025-08-05 05:28:41 +0900

    課題１８

---
+++ src/kadai_18/Kato_Chapter18.java
@@ -0,0 +1,21 @@
+package kadai_18;
+
+public abstract class Kato_Chapter18 {
+
+	String familyName = "加藤";
+	String givenName;
+	String address;
+	
+	public void commonIntroduce(){
+		System.out.println("名前は" + familyName + givenName + "です。");
+		System.out.println("住所は東京都中野区〇×です。");
+	}
+	
+	abstract public	void eachIntroduce();
+	
+	public void	execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+	
+}
