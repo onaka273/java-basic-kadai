9d2587e5c1398dde11a8318d81d1b7c744df176e
onaka273 <onaka273@gmail.com>
2025-08-05 07:37:06 +0900

    0805

---
+++ src/kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,22 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	
+	public Dictionary_Chapter21() {
+	
+	HashMap<String , String> dictionaryMap = new HashMap<String , String>();
+	dictionaryMap.put("apple" , "りんご");
+	dictionaryMap.put( "peach", "桃");
+	dictionaryMap.put( "banana", "バナナ");
+	dictionaryMap.put( "lemon", "レモン");
+	dictionaryMap.put( "pear", "梨");
+	dictionaryMap.put( "kiwi", "キウィ");
+	dictionaryMap.put( "strawberry", "いちご");
+	dictionaryMap.put( "grape", "ぶどう");
+	dictionaryMap.put( "muscat", "マスカット");
+	dictionaryMap.put( "cherry", "さくらんぼ");
+	
+	}
+}
