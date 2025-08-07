9c095e45c44b26436efc4ab08994da026b8c0ae2
onaka273 <onaka273@gmail.com>
2025-08-08 03:51:24 +0900

    住所の処理修正

--- src/kadai_018/Kato_Chapter18.java
+++ src/kadai_018/Kato_Chapter18.java
@@ -4,11 +4,11 @@ public abstract class Kato_Chapter18 {
 
 	String familyName = "加藤";
 	String givenName;
-	String address;
+	String address = "東京都中野区〇×";
 	
 	public void commonIntroduce(){
 		System.out.println("名前は" + familyName + givenName + "です。");
-		System.out.println("住所は東京都中野区〇×です。");
+		System.out.println("住所は" + address + "です。");
 	}
 	
 	abstract public	void eachIntroduce();
