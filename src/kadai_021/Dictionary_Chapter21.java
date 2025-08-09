0cee35f5bc7f935f68f162a70972464bc7131658
onaka273 <onaka273@gmail.com>
2025-08-10 04:17:58 +0900

    kadai21

--- src/kadai_021/Dictionary_Chapter21.java
+++ src/kadai_021/Dictionary_Chapter21.java
@@ -3,33 +3,33 @@ package kadai_021;
 import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
-		
-		private HashMap<String, String> dictionaryMap;
-	
-		public Dictionary_Chapter21() {
-	dictionaryMap.put("apple" , "りんご");
-	dictionaryMap.put( "peach", "桃");
-	dictionaryMap.put( "banana", "バナナ");
-	dictionaryMap.put( "lemon", "レモン");
-	dictionaryMap.put( "pear", "梨");
-	dictionaryMap.put( "kiwi", "キウィ");
-	dictionaryMap.put( "strawberry", "いちご");
-	dictionaryMap.put( "grape", "ぶどう");
-	dictionaryMap.put( "muscat", "マスカット");
-	dictionaryMap.put( "cherry", "さくらんぼ");
-	
+
+	private HashMap<String, String> dictionaryMap;
+
+	public Dictionary_Chapter21() {
+		dictionaryMap = new HashMap<String, String>();
+		dictionaryMap.put("apple", "りんご");
+		dictionaryMap.put("peach", "桃");
+		dictionaryMap.put("banana", "バナナ");
+		dictionaryMap.put("lemon", "レモン");
+		dictionaryMap.put("pear", "梨");
+		dictionaryMap.put("kiwi", "キウィ");
+		dictionaryMap.put("strawberry", "いちご");
+		dictionaryMap.put("grape", "ぶどう");
+		dictionaryMap.put("muscat", "マスカット");
+		dictionaryMap.put("cherry", "さくらんぼ");
+
 	}
 
-
 	public void referer(String[] wordArray) {
-        for (String word : wordArray) {
-            if (dictionaryMap.containsKey(word)) {
-                String meaning = dictionaryMap.get(word);
-                System.out.println(word + "の意味は" + meaning);
-            } else {
-                System.out.println(word + "は辞書に存在しません");
-            }
-        }
-    }	
-	
+		for (String word : wordArray) {
+			if (dictionaryMap.containsKey(word)) {
+				String meaning = dictionaryMap.get(word);
+				System.out.println(word + "の意味は" + meaning);
+			} else {
+				System.out.println(word + "は辞書に存在しません");
+			}
+		}
+	}
+
 }
